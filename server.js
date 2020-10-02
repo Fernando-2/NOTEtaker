34a45a6d518ffe2796eaa6e397d9019bafbca3e6
Fernando Rivas <fernandorivascastillo011@gmail.com>
2020-10-02 15:42:29 -0400

    almost there

--- server.js
+++ server.js
@@ -19,31 +19,41 @@ app.get("/api/notes", (req, res) => {
 
 app.post("/api/notes", function (req, res) {
     // gets the new note data from the req body.
-  const newNote = req.body;
-  // reads existing notes.
-  const dbNotes = fs.readFileSync("./db/db.json",);
-  // parses the raw data 
-  const notes = JSON.parse(dbNotes);
-  // creates array that uses (...) to hold old array data and makes new array.
-  const newNotes = [...notes, newNote];
-  console.log(newNotes);
-  // write new data to add to existing file.
-  fs.writeFileSync("./db/db.json", JSON.stringify(newNotes));
-  res.json(newNotes);
+    let newNote = req.body;
+    // reads existing notes.
+    const dbNotes = fs.readFileSync("./db/db.json",);
+    // parses the raw data 
+    const notes = JSON.parse(dbNotes);
+    //makes a new note id for every note made
+    newNote.id = notes.length 
+    console.log(newNote.id);
+    // creates array that uses (...) to hold old array data and makes new array.
+    const newNotes = [...notes, newNote];//notes.concat(newNote) is what this is doing
+    console.log(newNotes);
+    // write new data to add to existing file.
+    fs.writeFileSync("./db/db.json", JSON.stringify(newNotes));//will need this for delete down below
+    res.json(newNotes);
 });
-app.delete("/api/notes", (req, res) => {
-    
-    const deleteNote = req.body;
+app.delete("/api/notes/:id", (req, res) => {
+    //gets chosen id when clicked on
+    const chosen = req.params.id
+    console.log(chosen);
     // read current file
     const dbNotes = fs.readFileSync("./db/db.json");
     // parse raw data
     const newNotes = JSON.parse(dbNotes);
-    
+
     console.log(newNotes);
-    
+    for (var i = 0; i < chosen; i++) {
+        // create new array that holds data minus deleted index
+        if (chosen) {
+            chosen.splice(i, 1);
+        }
+        console.log(newNotes);
+    }
     // writes updated data to file eventually
-    res.json(deletedNotes);
-  });
+    res.json(newNotes);
+});
 //html routes
 app.use(express.static("public"));
 
